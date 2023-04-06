3ec19e70d979301ef6d6f8b8f8e257affd5fb2ba
Artur Nilson <arturnilson@gmail.com>
2023-04-06 11:15:40 -0300

    Criação de projeto utilizando typescript, node e mongo

---
+++ controller/newsController.ts
@@ -0,0 +1,87 @@
+import NewsService from "../services/newsService";
+import * as HttpStatus from "http-status";
+import * as redis from "redis";
+ 
+import Helper from "../infra/helper";
+ 
+class NewsController {
+  async get(req, res) {
+    let client = redis.createClient();
+ 
+    await client.get("news", async function (err, reply) {
+ 
+      try {
+ 
+        if (reply) {
+          console.log("redis");
+          Helper.sendResponse(res, HttpStatus.OK, JSON.parse(reply));
+ 
+        } else {
+          console.log("db");
+ 
+          let response = await NewsService.get();
+ 
+          client.set("news", JSON.stringify(response));
+ 
+          client.expire("news", 20);
+          
+          Helper.sendResponse(res, HttpStatus.OK, response);
+        }
+      } catch (error) {
+        console.error(error);
+      }
+ 
+    });
+  }
+ 
+  async getById(req, res) {
+ 
+    try {
+      const _id = req.params.id;
+      let response = await NewsService.getById(_id);
+      Helper.sendResponse(res, HttpStatus.OK, response);
+ 
+    } catch (error) {
+      console.error(error);
+    }
+ 
+  }
+ 
+  async create(req, res) {
+    try {
+      let vm = req.body;
+      await NewsService.create(vm);
+      Helper.sendResponse(res, HttpStatus.OK, "Noticia cadastrada com sucesso!");
+    } catch (error) {
+      console.error(error);
+    }
+ 
+  }
+ 
+  async update(req, res) {
+    try {
+      const _id = req.params.id;
+      let news = req.body;
+      await NewsService.update(_id, news);
+      Helper.sendResponse(res, HttpStatus.OK, `Noticia atualiza com sucesso!`);
+ 
+    } catch (error) {
+      console.error(error);
+    }
+ 
+  }
+ 
+  async delete(req, res) {
+    try {
+      const _id = req.params.id;
+      await NewsService.delete(_id);
+      Helper.sendResponse(res, HttpStatus.OK, "Noticia deletada com sucesso!");
+ 
+    } catch (error) {
+      console.error(error);
+    }
+ 
+  }
+}
+ 
+export default new NewsController();
---
+++ services/newsService.ts
@@ -0,0 +1,30 @@
+import NewsRepository from "../repository/newsRepository";
+
+class NewsService {
+  async get() {
+    let result = await NewsRepository.find({});
+    return result;
+  }
+
+  async getById(_id) {
+    let result = await NewsRepository.findById(_id);
+    return result;
+  }
+
+  async create(news) {
+    let result = await NewsRepository.create(news);
+    return result;
+  }
+
+  async update(_id, news) {
+    let result = await NewsRepository.findByIdAndUpdate(_id, news);
+    return result;
+  }
+
+  async delete(_id) {
+    let result = await NewsRepository.findByIdAndRemove(_id);
+    return result;
+  }
+}
+
+export default new NewsService();
