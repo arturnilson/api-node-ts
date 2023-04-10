474fbb52256a4b4c1d00d62a2785036dbfcf2273
Artur Nilson <arturnilson@gmail.com>
2023-04-10 13:38:25 -0300

    adicionando opção para download de csv

--- startUp.ts
+++ startUp.ts
@@ -33,6 +33,9 @@ class StartUp {
     this.enableCors();
     this.app.use(bodyParser.json());
     this.app.use(bodyParser.urlencoded({ extended: false }));
+
+    //Permite download de arquivos estáticos
+    this.app.use('/exports', express.static(process.cwd() + '/exports'));
   }
 
   routes() {
